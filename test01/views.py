c93c7b690f94d27c1b20b2a939cee9490c66812b
guywithglass <vishalsinghdewas@gmail.com>
2023-10-30 00:02:21 +0530

    29_10_2023

--- test01/views.py
+++ test01/views.py
@@ -7,9 +7,11 @@ def index(request):
 
 def response(request):
     aname = request.POST.get('name')
+    aphone = request.POST.get('phone')
     alocation = request.POST.get('location')
-    print(aname, alocation)
-    look = {'name':aname, 'location':alocation}
+    # print(aname, alocation)
+    look = {'name':aname, 'phone': aphone, 'location':alocation}
+    print(look)
     return render(request, 'response.html', look)
 
 def login(request):
